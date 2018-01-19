21b439ecfdcc703a1fbe4a198416c1a3ab041e33
Ryan McCue <me@ryanmccue.info>
2018-01-19 15:51:15 +1000

    Use className instead of class

--- src/Footer.js
+++ src/Footer.js
@@ -24,11 +24,11 @@ const DOCS = [
 ];
 
 export default function Footer( props ) {
-	return <footer class="Footer">
-		<div class="wrapper">
+	return <footer className="Footer">
+		<div className="wrapper">
 			<div>
 				<Logo />
-				<p>Created with <span class="Footer-heart">&hearts;</span> just for you.</p>
+				<p>Created with <span className="Footer-heart">&hearts;</span> just for you.</p>
 				<p>Reuse these docs under a <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p>
 			</div>
 			<NavMenu
