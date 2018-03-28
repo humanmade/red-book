dcd9c44da1359183d34dee9937746c8f9f7a3552
Ryan McCue <me@ryanmccue.info>
2018-03-28 11:12:19 +1000

    Add SSR framework

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ const SCRIPT_ID = 'redbook';
 require __DIR__ . '/inc/api/namespace.php';
 require __DIR__ . '/inc/assets/namespace.php';
 require __DIR__ . '/inc/contents/namespace.php';
+require __DIR__ . '/inc/ssr/namespace.php';
 require __DIR__ . '/inc/class-array-walker-nav-menu.php';
 require __DIR__ . '/inc/namespace.php';
 
