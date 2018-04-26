cabbf1c62496d412f06faadedd048d623dd81e4e
Ryan McCue <me@ryanmccue.info>
2018-04-26 13:45:37 +1000

    Ensure markup is correct for search container

--- src/Search.js
+++ src/Search.js
@@ -14,15 +14,17 @@ export default function Search( props ) {
 		return <div>No results found</div>;
 	}
 
-	return <ol className="Search markdown-body">
+	return <main className="Search markdown-body">
 		<h1>Search Results</h1>
-		{ props.posts.map( result =>
-			<li key={ result.id }>
-				<h2><Link href={ result.link }>{ result.title.rendered }</Link></h2>
-				<div
-					dangerouslySetInnerHTML={ { __html: result.excerpt.rendered } }
-				/>
-			</li>
-		) }
-	</ol>;
+		<ol>
+			{ props.posts.map( result =>
+				<li key={ result.id }>
+					<h2><Link href={ result.link }>{ result.title.rendered }</Link></h2>
+					<div
+						dangerouslySetInnerHTML={ { __html: result.excerpt.rendered } }
+					/>
+				</li>
+			) }
+		</ol>
+	</main>;
 }
